acc2c13968aa9d3ba9990bc6a33a95f3d9f737e2
Sharon <sharon.febre@gmail.com>
2017-11-12 05:52:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const currentUser = 'Grace Hopper';
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
-
 ...
 
 const shortGreeting = '';
